198a73a82dcd1b5e0cb4d23684cfb560967cac98
Fernando Giraldo <fernando.gm8@outlook.com>
2021-10-01 15:01:36 -0500

    adding file for creating s3 with ptyhon

---
+++ code/s3bucket.py
@@ -0,0 +1,20 @@
+#/usr/bin/python
+import boto3
+import sys
+
+try:
+    def main():
+        create_s3bucket(bucket_name)
+
+except Exception as e:
+    print(e)
+
+def create_s3bucket(bucket_name):
+    s3_bucket=boto3.client("s3")
+    bucket = s3_bucket.create_bucket(Bucket = bucket_name)
+    print (bucket)
+
+bucket_name = sys.argv[1]
+
+if __name__ == '__main__':
+    main()
