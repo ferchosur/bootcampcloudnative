d82510c2bc95f2cd07746eac878644cbde9bd1ce
ferchosur <ferchosur@gmail.com>
2021-10-27 20:45:32 -0500

    .

--- code/s3bucket.py
+++ code/s3bucket.py
@@ -1,20 +1,19 @@
 #/usr/bin/python
-import boto3
 import sys
+import boto3
 
 try:
     def main():
+        bucket_name = sys.argv[1]
         create_s3bucket(bucket_name)
 
-except Exception as e:
-    print(e)
+except Exception as error_catch:
+    print(error_catch)
 
 def create_s3bucket(bucket_name):
-    s3_bucket=boto3.client("s3")
-    bucket = s3_bucket.create_bucket(Bucket = bucket_name)
-    print (bucket)
-
-bucket_name = sys.argv[1]
+    s3_bucket = boto3.client("s3")
+    bucket = s3_bucket.create_bucket(Bucket=bucket_name)
+    print(bucket)
 
 if __name__ == '__main__':
     main()
